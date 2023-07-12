a6013fb40beaf14466b35e94b4d40f70616b87ba
jyu <yjs01016@naver.com>
2023-07-13 00:34:02 +0900

    삭제 확인 창 위치 변경

--- src/Article.js
+++ src/Article.js
@@ -11,19 +11,21 @@ export default function Article(props) {
   let navigate = useNavigate();
 
   return (
-    <main className='Article'>
-      <h2 className='article__title'>{article.title}</h2>
-      <p className='article__date'>{article.date}</p>
-      <hr/>
-      <div className='article__content'>{article.content}</div>
-      <div className={styles.article__btns}>
-        <button className={styles.btns__prevBtn} onClick={ () => navigate(-1) }>목록으로</button>
-        <div className={styles.btns__crtlBtns}>
-          <button className={styles.btns__updateBtn} onClick={ () => navigate(-1) }>수정하기</button>
-          <button className={styles.btns__deleteBtn} onClick={ () => setShowDeleteModal(!showDeleteModal) }>삭제하기</button>
-        </div>
-      </div>
+    <div>
       {showDeleteModal && <DeleteModal/>}
-    </main>
+      <main className='Article'>
+        <h2 className='article__title'>{article.title}</h2>
+        <p className='article__date'>{article.date}</p>
+        <hr/>
+        <div className='article__content'>{article.content}</div>
+        <div className={styles.article__btns}>
+          <button className={styles.btns__prevBtn} onClick={ () => navigate(-1) }>목록으로</button>
+          <div className={styles.btns__crtlBtns}>
+            <button className={styles.btns__updateBtn} onClick={ () => navigate(-1) }>수정하기</button>
+            <button className={styles.btns__deleteBtn} onClick={ () => setShowDeleteModal(!showDeleteModal) }>삭제하기</button>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
