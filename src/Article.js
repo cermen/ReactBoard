8b1ad61af8e5e1353e67eb4fb600af375a08aed6
cermen <yjs01016@naver.com>
2023-07-19 10:43:49 +0900

    모달창 주변 검은색 배경 지정

--- src/Article.js
+++ src/Article.js
@@ -9,7 +9,7 @@ export default function Article({ articles, handleArticles }) {
 
   const navigate = useNavigate();
 
-  const backToMain = () => navigate('/');    // 메인 페이지로 이동
+  const backToMain = () => navigate("/"); // 메인 페이지로 이동
   const toggleModal = () => setShowDeleteModal(!showDeleteModal); // 삭제 확인 창 띄우기/숨기기
 
   // 글 삭제하기 (Delete)
@@ -17,33 +17,48 @@ export default function Article({ articles, handleArticles }) {
     const newArticles = articles.filter((a) => a.id !== Number(id));
     backToMain();
     handleArticles(newArticles);
-  }
+  };
 
   function DeleteModal() {
     return (
-      <div className={styles.deleteModal}>
-        <p className={styles.deleteModal__msg}>게시글을 삭제하시겠습니까?</p>
-        <div className={styles.deleteModal__buttons}>
-          <button className={styles.buttons__yes} onClick={deleteArticle}>예</button>
-          <button className={styles.buttons__no} onClick={toggleModal}>아니오</button>
+      <div className={styles.overlay}>
+        <div className={styles.deleteModal}>
+          <p className={styles.deleteModal__msg}>게시글을 삭제하시겠습니까?</p>
+          <div className={styles.deleteModal__buttons}>
+            <button className={styles.buttons__yes} onClick={deleteArticle}>
+              예
+            </button>
+            <button className={styles.buttons__no} onClick={toggleModal}>
+              아니오
+            </button>
+          </div>
         </div>
       </div>
     );
   }
-  
+
   return (
     <div>
-      {showDeleteModal && <DeleteModal/>}
-      <main className='Article'>
-        <h2 className='article__title'>{article.title}</h2>
-        <p className='article__date'>{article.date}</p>
-        <hr/>
-        <div className='article__content'>{article.content}</div>
+      {showDeleteModal && <DeleteModal />}
+      <main className="Article">
+        <h2 className="article__title">{article.title}</h2>
+        <p className="article__date">{article.date}</p>
+        <hr />
+        <div className="article__content">{article.content}</div>
         <div className={styles.article__btns}>
-          <button className={styles.btns__prevBtn} onClick={backToMain}>목록으로</button>
+          <button className={styles.btns__prevBtn} onClick={backToMain}>
+            목록으로
+          </button>
           <div className={styles.btns__crtlBtns}>
-            <button className={styles.btns__updateBtn} onClick={() => navigate(`/write/${id}`)}>수정하기</button>
-            <button className={styles.btns__deleteBtn} onClick={toggleModal}>삭제하기</button>
+            <button
+              className={styles.btns__updateBtn}
+              onClick={() => navigate(`/write/${id}`)}
+            >
+              수정하기
+            </button>
+            <button className={styles.btns__deleteBtn} onClick={toggleModal}>
+              삭제하기
+            </button>
           </div>
         </div>
       </main>
